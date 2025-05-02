10e3bb0975009fe2081e237a587f9b7ef9e7b952
Piero Rolando <rolandopiero63@gmail.com>
2025-05-02 00:34:02 -0500

    zz

--- biblioteca/src/test/java/com/example/biblioteca_java/BibliotecaJavaApplicationTests.java
+++ biblioteca/src/test/java/com/example/biblioteca_java/BibliotecaJavaApplicationTests.java
@@ -4,10 +4,4 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BibliotecaJavaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+class BibliotecaJavaApplicationTests {}
