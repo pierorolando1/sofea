dc7ea1d07dd660293dab500cb123480b60dbb983
Piero Rolando <rolandopiero63@gmail.com>
2025-05-16 00:35:49 -0500

    backup

--- biblioteca/src/main/java/com/example/biblioteca/BibliotecaJavaApplication.java
+++ biblioteca/src/main/java/com/example/biblioteca/BibliotecaJavaApplication.java
@@ -2,10 +2,8 @@ package com.example.biblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class BibliotecaJavaApplication {
 
 	public static void main(String[] args) {
